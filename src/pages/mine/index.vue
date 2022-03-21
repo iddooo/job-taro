2d746ed03ea13afa3ea5191b76856ff2d0b441d2
janedeng <janedeng072@163.com>
2022-03-21 19:16:41 +0800

    我的

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,26 +1,121 @@
 <template>
   <view class="index">
-    <view class="index">
-    <AtNoticebar marquee>
-      欢迎使用 Taro UI Vue
-    </AtNoticebar>
-    <AtButton
-      type="primary"
-      :on-click="handleClick"
-    >
-      AtButton
-    </AtButton>
-    <AtToast :is-opened="show" :text="msg" :on-close="handleClose"></AtToast>
-  </view>
+    <view class="head">
+        <view class="user-info flex-ct-btw">
+            <view class="avater">
+                <image src=""></image>
+            </view>
+            <view class="user-name">登录开启赚钱旅程</view>
+            <view class="sign">
+                <image src=""></image>
+            </view>
+        </view>
+        
+        <view class="count flex-ct-btw">
+            <view class="c-item">
+                <view class="score">0.00 元</view>
+                <view class="name flex-ct">
+                    <view>账户余额</view>
+                  <image class="arrow-icon" src=""></image>
+                </view>
+            </view>
+            <view class="line"></view>
+            <view class="c-item">
+                <view class="score">0.00 元</view>
+                <view class="name flex-ct">
+                    <view>账户余额</view>
+                  <image class="arrow-icon" src=""></image>
+                </view>
+            </view>
+            <view class="line"></view>
+            <view class="c-item">
+                <view class="score">0.00 元</view>
+                <view class="name flex-ct">
+                    <view>账户余额</view>
+                  <image class="arrow-icon" src=""></image>
+                </view>
+            </view>
+        </view>
+    </view>
+
+    <view class="profile">
+        <image src=""></image>
+        <view class="percent flex-ct">完善度 <view class="green">72%</view>,完善简历可提高录取率~</view>
+    </view>
+
+    <view class="my-apply flex-ct">
+        <view class="apply-status">
+            <view class="count">0</view>
+            <view class="status">全部</view>
+        </view>
+        <view class="apply-status">
+            <view class="count">0</view>
+            <view class="status">已报名</view>
+        </view>
+        <view class="apply-status">
+            <view class="count">0</view>
+            <view class="status">已录取</view>
+        </view>
+        <view class="apply-status">
+            <view class="count">0</view>
+            <view class="status">已结束</view>
+        </view>
+    </view>
+
+    <view class="menu">
+        <view class="menu-item flex-ct-btw">
+            <view class="flex-ct">
+                <view class="menu-icon">
+                    <image src=""></image>
+                </view>
+                <view class="menu-name">我的收藏</view>
+            </view>
+            <view class="flex-ct-end">
+                <view class="count">0</view>
+                <image class="menu-arrow" src=""></image>
+            </view>
+        </view>
+        <view class="menu-item flex-ct-btw">
+            <view class="flex-ct">
+                <view class="menu-icon">
+                    <image src=""></image>
+                </view>
+                <view class="menu-name">我要招聘</view>
+            </view>
+            <view class="flex-ct-end">
+                <image class="menu-arrow" src=""></image>
+            </view>
+        </view>
+        <view class="menu-item flex-ct-btw">
+            <view class="flex-ct">
+                <view class="menu-icon">
+                    <image src=""></image>
+                </view>
+                <view class="menu-name">服务中心</view>
+            </view>
+            <view class="flex-ct-end">
+                <image class="menu-arrow" src=""></image>
+            </view>
+        </view>
+        <view class="menu-item flex-ct-btw">
+            <view class="flex-ct">
+                <view class="menu-icon">
+                    <image src=""></image>
+                </view>
+                <view class="menu-name">用户设置</view>
+            </view>
+            <view class="flex-ct-end">
+                <image class="menu-arrow" src=""></image>
+            </view>
+        </view>
+    </view>
   </view>
 </template>
 
 <script>
 // 按需引入, 更小的应用体积
-import { AtButton, AtToast, AtNoticebar } from 'taro-ui-vue'
+import { AtButton } from 'taro-ui-vue'
 import "taro-ui-vue/dist/style/components/button.scss"
-import "taro-ui-vue/dist/style/components/toast.scss"
-import "taro-ui-vue/dist/style/components/noticebar.scss"
 import './index.scss' 
 
 definePageConfig({
@@ -29,23 +124,13 @@ definePageConfig({
 
 export default {
     components: {
-    AtButton,
-    AtToast,
-    AtNoticebar
+    AtButton
   },
   data () {
     return {
-      msg: 'Hello world!',
-      show: false
     }
   },
   methods: {
-    handleClick () {
-      this.show = true
-    },
-    handleClose () {
-      this.show = false
-    }
   },
     }
 </script>
--- src/pages/notice/index.vue
+++ src/pages/notice/index.vue
@@ -20,6 +20,16 @@
               <view class="font3">一大波企业正在招聘，福利多多，快来看看新的职位是否有你需要的吧～</view>
           </view>
       </view>
+
+      <view class="notice">
+          <view class="time flex-ct-ct">8月2日 上午 10:00</view>
+          <view class="info">
+              <view class="font1">8月5日 系统通知</view>
+              <view class="font2">猎头推荐职位</view>
+              <view class="font3">一大波企业正在招聘，福利多多，快来看看新的职位是否有你需要的吧～</view>
+              <image class="cover" src=""></image>
+          </view>
+      </view>
   </view>
 </template>
 
